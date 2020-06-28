71d72a7ca57477d5301dccc6ecf3c36e50bcf948
Marco <marco@defrostudio.eu>
2020-06-28 17:58:51 +0200

    revert

--- freeze.php
+++ freeze.php
@@ -23,7 +23,6 @@
             <button class="button" id="capture">
               <p>to own is to interact</p>
             </button>
-            <?php echo do_shortcode("[contact-form-7 id="77" title=""]"); ?>
           </div>
         </div>
         <div class="buttons">
