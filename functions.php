0adcebcf3383b782dc821474725477501212871d
Marco <marco@defrostudio.eu>
2020-06-29 12:34:26 +0200

    fix

--- functions.php
+++ functions.php
@@ -153,7 +153,7 @@ function buycloud_scripts() {
 
 	wp_enqueue_script('buycloud-script', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), _S_VERSION, true );
 
-	wp_enqueue_script('buycloud-script', get_template_directory_uri() . '/js/html2canvas.min.js', array( '' ), _S_VERSION, true );
+	wp_enqueue_script('buycloud-script', get_template_directory_uri() . '/js/html2canvas.min.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
