86aac3a80badae08f342d71e564162ed31db9a98
Marco <marco@defrostudio.eu>
2020-06-29 12:34:16 +0200

    html2canvas

--- functions.php
+++ functions.php
@@ -151,7 +151,9 @@ function buycloud_scripts() {
 
 	wp_enqueue_script( 'buycloud-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
-	wp_enqueue_script('tu-berlin-script', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), _S_VERSION, true );
+	wp_enqueue_script('buycloud-script', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), _S_VERSION, true );
+
+	wp_enqueue_script('buycloud-script', get_template_directory_uri() . '/js/html2canvas.min.js', array( '' ), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
