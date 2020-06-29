accdfeb41dc4334c620d49626ce37804a8bb65cb
Marco <marco@defrostudio.eu>
2020-06-29 13:57:56 +0200

    fixed feed

--- freeze-cloud.php
+++ freeze-cloud.php
@@ -96,27 +96,25 @@
       <video id="player" controls autoplay style="display: none;"></video>
 
       <script>
-        const player = document.getElementById("player");
-        const canvas = document.getElementById("canvas");
-        const context = canvas.getContext("2d");
-        const captureButton = document.getElementById("capture");
+        const player = document.getElementById('player');
+        const canvas = document.getElementById('canvas');
+        const context = canvas.getContext('2d');
+        const captureButton = document.getElementById('capture');
 
         const constraints = {
-          video: true
+          video: true,
         };
 
-        captureButton.addEventListener("click", () => {
+        captureButton.addEventListener('click', () => {
+          // Draw the video frame to the canvas.
           context.drawImage(player, 0, 0, canvas.width, canvas.height);
-
-        
-
-        // Attach the video stream to the video element and autoplay.
-        navigator.mediaDevices.getUserMedia(constraints).then(stream => {
-          player.srcObject = stream;
         });
 
-        // save canvas image as data url (png format by default)
-        var dataURL = canvas.toDataURL();
+        // Attach the video stream to the video element and autoplay.
+        navigator.mediaDevices.getUserMedia(constraints)
+          .then((stream) => {
+            player.srcObject = stream;
+          });
       </script>
     </div>
 
