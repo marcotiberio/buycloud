cadae95b6d7ba86f1ce811647a6d68b309d5c6be
Marco <marco@defrostudio.eu>
2020-07-03 13:40:14 +0200

    fix

--- freeze-cloud.php
+++ freeze-cloud.php
@@ -49,9 +49,9 @@
             <span><label for="certificate">take a picture while gazing at your cloud</label></span>
             <div class="test">
               <div>
-                <label for="name">name:</label></span>
-                <label for="email">email:</label></span>
-                <label for="confirm">confirm email:</label></span>
+                <label for="name">name:</label>
+                <label for="email">email:</label>
+                <label for="confirm">confirm email:</label>
               </div>
               <div>
                 <input type="text" id="name" name="name" value="John" style="text-align: right; padding: 0; margin-right: 10px;">
