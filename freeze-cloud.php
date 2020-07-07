38443bc31fcabe3cb62c6c1d764a121d7c16c45d
Marco <marco@defrostudio.eu>
2020-07-07 09:56:42 +0200

    revert

--- freeze-cloud.php
+++ freeze-cloud.php
@@ -69,27 +69,6 @@ get_header(); ?>
           <?php the_field('paypal_button'); ?>
         </button>
 
-
-        <div id="content">
-	
-          <?php
-          
-          acf_form(array(
-            'post_id'		=> 'new_post',
-            'post_title'	=> true,
-            'post_content'	=> true,
-            'new_post'		=> array(
-              'post_type'		=> 'contact_form',
-              'post_status'	=> 'publish'
-            ),
-            'return'		=> home_url('contact-form-thank-you'),
-            'submit_value'	=> 'Send'
-          ));
-          
-          ?>
-          
-        </div>
-
         
       </main>
 
--- functions.php
+++ functions.php
@@ -163,49 +163,6 @@ add_action( 'wp_enqueue_scripts', 'buycloud_scripts' );
 
 
 
-add_action('acf/save_post', 'my_save_post');
-
-function my_save_post( $post_id ) {
-	
-	// bail early if not a contact_form post
-	if( get_post_type($post_id) !== 'contact_form' ) {
-		
-		return;
-		
-	}
-	
-	
-	// bail early if editing in admin
-	if( is_admin() ) {
-		
-		return;
-		
-	}
-	
-	
-	// vars
-	$post = get_post( $post_id );
-	
-	
-	// get custom fields (field group exists for content_form)
-	$name = get_field('name', $post_id);
-	$email = get_field('email', $post_id);
-	
-	
-	// email data
-	$to = 'noajansma@gmail.com';
-	$headers = 'From: ' . $name . ' <' . $email . '>' . "\r\n";
-	$subject = $post->post_title;
-	$body = $post->post_content;
-	
-	
-	// send email
-	wp_mail($to, $subject, $body, $headers );
-	
-}
-
-
-
 /**
  * Implement the Custom Header feature.
  */
