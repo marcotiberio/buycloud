9594f4847259a03e6e7481ca8b3b4b964ab747c8
Marco <marco@defrostudio.eu>
2020-07-07 09:52:06 +0200

    test form

--- freeze-cloud.php
+++ freeze-cloud.php
@@ -1,8 +1,9 @@
 <?php 
 	/* Template Name: Freeze Cloud */
-?>
 
-<?php get_header(); ?>
+acf_form_head();
+
+get_header(); ?>
 
 
 <div class="post-header">
@@ -67,6 +68,27 @@
         <button class="button" id="payment">
           <?php the_field('paypal_button'); ?>
         </button>
+
+
+        <div id="content">
+	
+          <?php
+          
+          acf_form(array(
+            'post_id'		=> 'new_post',
+            'post_title'	=> true,
+            'post_content'	=> true,
+            'new_post'		=> array(
+              'post_type'		=> 'contact_form',
+              'post_status'	=> 'publish'
+            ),
+            'return'		=> home_url('contact-form-thank-you'),
+            'submit_value'	=> 'Send'
+          ));
+          
+          ?>
+          
+        </div>
 
         
       </main>
