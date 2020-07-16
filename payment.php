3c98ec1250c99fb33b2e45f55ca3aab43085771b
Marco <marco@defrostudio.eu>
2020-07-16 18:40:33 +0200

    text

--- payment.php
+++ payment.php
@@ -8,7 +8,17 @@ get_header(); ?>
   <img src="https://www.buycloud.space/wp-content/uploads/2020/07/postheader1.png" alt="">
 </div>
     <section class="main-content">
-      
+      <p>
+      Your certifiacte will be send to your email once it is generated. 
+      This can take some time. Your ownership is implemented in the virtual 
+      cadaste in the Universal System (US ). It will forever roam with its 
+      frozen values. It can gain power by concurring smaller clouds, but the 
+      opposite can happen too. Check in on the US to see what’s happening to 
+      your cloud. The overall value of the clouds will rise in connection to 
+      the increasing Co2-emissions as this will manipulate the scarcity and 
+      demand of clouds. Acces and track your cloud regurlarly in the US on your 
+      desktop with the link in your e-mail, or go to BUYCLOUD.space on your computer.
+      </p>
     </section>
 
     
