842d8b0c1ec66402008b409b99d8ec7cad89dc33
Marco <marco@defrostudio.eu>
2020-07-16 18:38:48 +0200

    payment template

---
+++ payment.php
@@ -0,0 +1,18 @@
+<?php 
+	/* Template Name: Payment */
+
+get_header(); ?>
+
+
+<div class="post-header-product">
+  <img src="https://www.buycloud.space/wp-content/uploads/2020/07/postheader1.png" alt="">
+</div>
+    <section class="main-content">
+      
+    </section>
+
+    
+    
+<?php
+get_sidebar();
+get_footer();
