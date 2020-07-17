a94e052a8fa154067bb9fcde1ccaf7c8e3cbfe92
Marco <marco@defrostudio.eu>
2020-07-17 09:23:49 +0200

    post-header payment

--- payment.php
+++ payment.php
@@ -5,7 +5,7 @@ get_header(); ?>
 
 
 <div class="post-header-product">
-  <img src="https://www.buycloud.space/wp-content/uploads/2020/07/postheader1.png" alt="">
+  <img src="https://www.buycloud.space/wp-content/uploads/2020/07/Group-42.png" alt="">
 </div>
     <section class="main-content">
       <p>
