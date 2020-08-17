1fffdb4ae834e8077b613c999647b5f7bb9a2140
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-08-17 15:35:29 +0200

    souldout template

---
+++ soldout.php
@@ -0,0 +1,18 @@
+<?php 
+	/* Template Name: Soldout */
+
+get_header(); ?>
+
+
+<div class="post-header-product">
+  <img style="max-width: 80vw;" src="https://www.buycloud.space/wp-content/uploads/2020/08/soldout.png" alt="">
+</div>
+    <section class="main-content">
+      
+    </section>
+
+    
+    
+<?php
+get_sidebar();
+get_footer();
