85c6a17ad00a16ad165d62b7d9b636a687117f57
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-08-20 12:39:37 +0200

    declaration soldOut

--- soldout.php
+++ soldout.php
@@ -8,7 +8,7 @@ get_header(); ?>
   <img style="max-width: 80vw;" src="https://www.buycloud.space/wp-content/uploads/2020/08/soldout.png" alt="">
 </div>
     <section class="main-content">
-      <p id="declaration">
+      <p id="declarationSoldout">
         read declaration
       </p>
     </section>
