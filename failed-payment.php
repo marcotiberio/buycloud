ad38caa743d3eac35f73411b60ba19f1232e0208
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-10-21 12:02:21 +0200

    failed template

---
+++ failed-payment.php
@@ -0,0 +1,34 @@
+<?php 
+	/* Template Name: Payment */
+
+get_header(); ?>
+
+
+<div class="post-header-product">
+  <img style="max-width: 80vw;" src="https://www.buycloud.space/wp-content/uploads/2020/08/thankyou.png" alt="">
+</div>
+    <section class="main-content">
+      <p>
+      If the problem keeps occuring send an e-mail to 
+      <a href="mailto:urcloud@buycloud.space" target="_blank">urcloud@buycloud.space</a>
+      <br><br>
+      Your ownership is implemented in the virtual 
+      cadastre in the Universal System (<span style="font-family: Baskerville; font-style: italic; font-weight: bold; font-size: 1rem; line-height: 1rem; color: #E92828;">US</span>). It will forever roam with its 
+      frozen values. It can gain power by concurring smaller clouds, but the 
+      opposite can happen too. Check in on the US to see what’s happening to 
+      your cloud. 
+      <br><br>
+      The overall value of the clouds will rise in connection to 
+      the increasing Co2-emissions as this will manipulate the scarcity and 
+      demand of clouds. 
+      <br><br>
+      Acces and track your cloud regularly in the US on your 
+      desktop with the link in your e-mail, or go to <span style="font-family: Baskerville; font-style: italic; font-weight: bold; font-size: 1rem; line-height: 1rem; color: #E92828;">BUYCLOUD.space</span> on your computer.
+      </p>
+    </section>
+
+    
+    
+<?php
+get_sidebar();
+get_footer();
